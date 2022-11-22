7db595971c64e9f12f01bfca5bbf0a91e262d132
shraddhabidaye <shraddhabidaye@gmail.com>
2022-11-22 20:15:45 +0530

    updated dreprecated functions

--- src/Stats/FlagStatsCount.php
+++ src/Stats/FlagStatsCount.php
@@ -10,24 +10,27 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\flag\Event\FlagEvents;
 use Drupal\flag\Event\FlaggingEvent;
 use Drupal\flag\Event\UnflaggingEvent;
+use Psr\Log\LoggerInterface;
 
 class FlagStatsCount implements EventSubscriberInterface {
+
   // Call database entry on flaging of entity.
   public function onFlag(FlaggingEvent $event) {
     $flagging = $event->getFlagging();
     $entity_nid = $flagging->getFlaggable()->id();
     $enity_type = $flagging->getFlaggable()->getEntityType()->id();
-    $user_id = $flagging->getFlaggable()->getCurrentUserId();
+    $user_id = $flagging->getFlaggable()->getOwnerId();
     $flag = $flagging->getFlag();
+    //  \Drupal::logger('entity_id_stat')->notice(print_r($user_id[0]));
     // Add flag statestics entry on entity flagged.
     if ($flag->getThirdPartySetting('flag_stats', 'flag_stat', NULL) == 1) {
-      $query = \Drupal::database()->insert('flag_stats');
-      $query->fields([
+       $query = \Drupal::database()->insert('flag_stats');
+       $query->fields([
         'status' => 1,
         'flag_type' => $flag->getOriginalId(),
         'entity_id' => $entity_nid,
         'entity_type' => $enity_type,
-        'uid' => $user_id[0],
+        'uid' => $user_id,
         'timestamp' => REQUEST_TIME,
       ])->execute();
     }
@@ -39,36 +42,36 @@ class FlagStatsCount implements EventSubscriberInterface {
     $flagging = reset($flagging);
     $entity_nid = $flagging->getFlaggable()->id();
     $enity_type = $flagging->getFlaggable()->getEntityType()->id();
-    $user_id = $flagging->getFlaggable()->getCurrentUserId();
+    $user_id = $flagging->getFlaggable()->getOwnerId();
     $flag = $flagging->getFlag();
 
     // Remove flag statestics entry on entity unflagged if option is enabled
     // in configuation.
     if ($flag->getThirdPartySetting('flag_stats', 'delete_flag_stat', NULL) == 1) {
-      $all_fids = db_select('flag_stats', 'f')
-        ->fields('f', array('fid'))
+      $all_fids = \Drupal::database()->select('flag_stats', 'f')
+        ->fields('f', ['fid'])
         ->condition('f.entity_id', $entity_nid, '=')
-        ->condition('f.uid', $user_id[0], '=')
+        ->condition('f.uid', $user_id, '=')
         ->execute()
         ->fetchAll();
-
+        // \Drupal::logger('entity_id_stat')->notice(print_r($all_fids));
       foreach ($all_fids as $fid) {
-        db_delete('flag_stats')
+        \Drupal::database()->delete('flag_stats')
           ->condition('fid', $fid->fid, '=')
           ->execute();
       }
     }
     elseif ($flag->getThirdPartySetting('flag_stats', 'unflag_stats', NULL) == 1) {
       // Add flag statestics entry on entity unflagged.
-      db_insert('flag_stats')
-        ->fields(array(
+      \Drupal::database()->insert('flag_stats')
+        ->fields([
           'status' => 0,
           'flag_type' => $flag->getOriginalId(),
           'entity_id' => $entity_nid,
           'entity_type' => $enity_type,
-          'uid' => $user_id[0],
+          'uid' => $user_id,
           'timestamp' => REQUEST_TIME,
-        ))->execute();
+        ])->execute();
     }
   }
 
